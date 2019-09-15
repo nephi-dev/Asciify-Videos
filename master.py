a32021a44a5e780d904f31d5fb79222a7248b322
Matheus Henrique <48372168+Nephilim-Jack@users.noreply.github.com>
2019-09-15 18:58:23 -0300

    Update master.py

--- master.py
+++ master.py
@@ -1,12 +1,17 @@
-from transform_to_ascii import runner
 from catch_frames import catch_video_frames
-from terminal_viewer import show_frame_in_terminal
+from terminal_viewer import create_image_from_frame
+from transform_in_video import create_video_from_frames
+from transform_to_ascii import runner
 
 frames_list = list()
 my_vid = str(input('Enter your video path: '))
 my_vid = my_vid.replace('\\', '/')
 
+frame_id = 1
 for frame in catch_video_frames(my_vid):
-    show_frame_in_terminal(runner(frame))
+    new_frame = create_image_from_frame(runner(frame))
+    frames_list.append(new_frame)
+
+create_video_from_frames(frames_list, my_vid)
 
 end = str(input('Press enter to Exit!'))
