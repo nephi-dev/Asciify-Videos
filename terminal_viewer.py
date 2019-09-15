5393afd65a60dcb93019242bffb2cd9cce19120b
Matheus Henrique <48372168+Nephilim-Jack@users.noreply.github.com>
2019-09-15 18:58:40 -0300

    Update terminal_viewer.py

--- terminal_viewer.py
+++ terminal_viewer.py
@@ -1,2 +1,9 @@
-def show_frame_in_terminal(frame_as_string):
-    print(frame_as_string)
+from PIL import Image, ImageDraw
+
+
+def create_image_from_frame(frame_as_string):
+    image = Image.new('RGB', (610, 850), color=(0, 0, 0))
+    drawing = ImageDraw.Draw(image)
+    drawing.text((0, 0), frame_as_string, fill=(255, 255, 255))
+    
+    return image
