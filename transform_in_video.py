84d2113c5d4164ca2b63ac8ba52591d22d4ff3c7
Matheus Henrique <48372168+Nephilim-Jack@users.noreply.github.com>
2019-09-15 20:06:20 -0300

    Update transform_in_video.py

--- transform_in_video.py
+++ transform_in_video.py
@@ -11,9 +11,10 @@ def create_video_from_frames(frames_list, original_video):
     video_fps = cv2.VideoCapture(original_video)
     video_fps = video_fps.get(cv2.CAP_PROP_FPS)
 
-    video_name = 'Asciified_' + original_video.split('/')[-1]
-    
-    video = cv2.VideoWriter(video_name, 0, video_fps, (610, 850))
+    video_name = 'Asciified_' + original_video.split('/')[-1].split('.')[0] + '.mp4'
+
+    force_mp4 = cv2.VideoWriter_fourcc(*'MP4')    
+    video = cv2.VideoWriter(video_name, force_mp4, video_fps, (610, 850))
 
     for frame in frames_list:
         video.write(convert_to_opencv(frame))
