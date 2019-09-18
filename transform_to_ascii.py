dbc8dd9ba5f110d5598f30ba7a4c56aae5242a8f
Matheus Henrique <48372168+Nephilim-Jack@users.noreply.github.com>
2019-09-18 20:54:12 -0300

    Update transform_to_ascii.py

--- transform_to_ascii.py
+++ transform_to_ascii.py
@@ -3,40 +3,41 @@ from PIL import Image
 ASCII_CHARS = ['.',',',':',';','+','*','?','%','S','#','@']
 ASCII_CHARS = ASCII_CHARS[::-1]
 
-'''
-method resize():
+
+def resize(image, new_width=100):
+    '''
     - takes as parameters the image, and the final width
     - resizes the image into the final width while maintaining aspect ratio
-'''
-def resize(image, new_width=100):
+    '''
+    
     (old_width, old_height) = image.size
     aspect_ratio = float(old_height)/float(old_width)
     new_height = int(aspect_ratio * new_width)
     new_dim = (new_width, new_height)
     new_image = image.resize(new_dim)
     return new_image
-'''
-method grayscalify():
+
+def grayscalify(image):
+    '''
     - takes an image as a parameter
     - returns the grayscale version of image
-'''
-def grayscalify(image):
+    '''
     return image.convert('L')
 
-'''
-method modify():
-    - replaces every pixel with a character whose intensity is similar
-'''
 def modify(image, buckets=25):
+    '''
+    - replaces every pixel with a character whose intensity is similar
+    '''
+    
     initial_pixels = list(image.getdata())
     new_pixels = [ASCII_CHARS[pixel_value//buckets] for pixel_value in initial_pixels]
     return ''.join(new_pixels)
 
-'''
-method do():
-    - does all the work by calling all the above functions
-'''
 def do(image, new_width=100):
+    '''
+    - does all the work by calling all the above functions
+    '''
+    
     image = resize(image)
     image = grayscalify(image)
 
